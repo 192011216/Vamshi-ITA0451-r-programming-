4e9c1b67467e4cf8a8e2327663e78baf447ddefb
Vamshi rangu <113601092+192011216@users.noreply.github.com>
2023-05-07 22:02:32 +0530

    Create 7-EX-2.R

---
+++ 7-EX-2.R
@@ -0,0 +1,20 @@
+# Load the trees dataset and make sure it is a data frame
+data(trees)
+if(!is.data.frame(trees)) {
+  trees <- as.data.frame(trees)
+}
+
+# Create the new data frame A
+A <- data.frame(
+  Girth = c(mean(trees$Girth), min(trees$Girth), max(trees$Girth), sum(trees$Girth)),
+  Height = c(mean(trees$Height), min(trees$Height), max(trees$Height), sum(trees$Height)),
+  Volume = c(mean(trees$Volume), min(trees$Volume), max(trees$Volume), sum(trees$Volume)),
+  row.names = c("mean_tree", "min_tree", "max_tree", "sum_tree")
+)
+A
+# Order the data frame by the first column
+A <- A[order(A[,1]),]
+A
+# Rename the row names
+row.names(A) <- c("mean", "min", "max", "tree")
+row.names(A)
