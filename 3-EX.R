a5123cbacb4903eb88e63dc8e617341752619452
Vamshi rangu <113601092+192011216@users.noreply.github.com>
2023-05-07 21:55:14 +0530

    Create 3-EX.R

---
+++ 3-EX.R
@@ -0,0 +1,11 @@
+names <- c("VAMSHI", "abhi","pawan",
+		"hitler","deva")
+print( "original data:")
+names
+# apply lapply() function
+print("data after lapply():")
+lapply(names, toupper)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
